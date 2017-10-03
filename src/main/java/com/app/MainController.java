cdc6d0dafc1c8e79617715b3c167deaa4054a309
mike.arguello@acelity.com <mike.arguello@acelity.com>
2017-10-03 07:28:22 -0500

    swagger updated

--- src/main/java/com/app/MainController.java
+++ src/main/java/com/app/MainController.java
@@ -18,20 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.business.PersonService;
 import com.app.entity.Person;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 
 @RestController    // using @RestController and not @Controller so we don't have to use @responsebody in our methods 
 @RequestMapping(path="/customer/v1") // This means URL's start with /demo (after Application path)
+@Api(value="Customer Master", description="Operations pertaining to Customer Master Data")
 public class MainController {
     
 	@Autowired
     private PersonService personService;
-        
-
-         
+     
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);	 
 
-
+    @ApiOperation(value = "Add a customer", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully added customer"),
+            @ApiResponse(code = 401, message = "You are not authorized to add the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })  
     @RequestMapping(value = "/add", method= RequestMethod.POST)  
 	public String addNewPerson (@RequestParam String name, @RequestParam String email) {
 		// @ResponseBody means the returned String is the response, not a view name
@@ -42,6 +51,13 @@ public class MainController {
 	}
         
 
+    @ApiOperation(value = "View a customer", response = Person.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved person"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })    
     @RequestMapping(value = "/getOne/{token}", method= RequestMethod.GET)
 	public Person getThisUser(@PathVariable Integer token) {
 		// @ResponseBody means the returned String is the response, not a view name
@@ -52,6 +68,13 @@ public class MainController {
                 
 	}
 
+    @ApiOperation(value = "View a list of customers", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @RequestMapping(value = "/getAll", method= RequestMethod.GET)
 	public Iterable<Person> getAllUsers() {
 		// This returns a JSON or XML with the users
@@ -59,6 +82,7 @@ public class MainController {
 		return personService.getAllUsers();
 	}
         
+    
     @RequestMapping(value = "/hi", method= RequestMethod.GET)
 	public String hello(@RequestParam String name) {
 		// This returns a JSON or XML with the users
