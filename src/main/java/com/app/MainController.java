60478cf02210586eef55e05056e95e9d5676a8ad
mike.arguello@gmail.com <mike.arguello@gmail.com>
2017-10-05 13:18:38 -0500

    foo

--- src/main/java/com/app/MainController.java
+++ src/main/java/com/app/MainController.java
@@ -53,7 +53,7 @@ public class MainController {
 
     @ApiOperation(value = "View a customer", response = Person.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully retrieved person"),
+            @ApiResponse(code = 200, message = "Successfully retrieved customer"),
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
