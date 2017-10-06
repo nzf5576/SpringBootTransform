ad188e1580bc34cc587b948f576babee7ff29697
root <root@Ryzen1>
2017-10-05 22:20:53 -0500

    changed timer interval to hours

--- src/main/java/com/app/timer/BasicTimer.java
+++ src/main/java/com/app/timer/BasicTimer.java
@@ -16,7 +16,7 @@ public class BasicTimer {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 500000)
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
         System.out.println("The time is now {}"+ dateFormat.format(new Date()));
