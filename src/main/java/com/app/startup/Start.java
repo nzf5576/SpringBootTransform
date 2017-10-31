78858f1ae6408d56c8a0e453e26c2af45df5abec
mike arguello <mike.arguello@gmailo.com>
2017-10-31 11:52:47 -0500

    logger mods

--- src/main/java/com/app/startup/Start.java
+++ src/main/java/com/app/startup/Start.java
@@ -2,15 +2,21 @@ package com.app.startup;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.app.MainController;
 
 @Component
 public class Start {
 
-
+	private static final Logger logger = LoggerFactory.getLogger(Start.class);	 
+	
     @PostConstruct
     public void init(){
         System.out.println("-------------------> Initializing Services");
-        
+        System.out.println(Start.class.getClassLoader().getResource("logging.properties"));
+        logger.info("--PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP-");
     }
 }
